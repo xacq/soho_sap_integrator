6db60fa4e4c502a640800c1288ba715f056f125c
xacq <tu-email@example.com>
2026-02-05 16:55:16 -0500

    30rd commit

--- obj/Debug/net8.0/SohoSapIntegrator.AssemblyInfo.cs
+++ obj/Debug/net8.0/SohoSapIntegrator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SohoSapIntegrator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+669300e367296c14d817aaf42a56114063643430")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30afef9d8410b2569a21016339b72d8c802544f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SohoSapIntegrator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SohoSapIntegrator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net8.0/SohoSapIntegrator.AssemblyInfo.cs
+++ obj/Release/net8.0/SohoSapIntegrator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SohoSapIntegrator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30afef9d8410b2569a21016339b72d8c802544f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SohoSapIntegrator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SohoSapIntegrator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
